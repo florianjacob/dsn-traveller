73764e4b7a53733003a82265386c7891a95abead
Florian Jacob <projects+git@florianjacob.de>
2018-07-26 00:22:01 +0200

    matrixgraph: add fn user_room_distribution(), apply cargo fmt

--- matrixgraph/src/lib.rs
+++ matrixgraph/src/lib.rs
@@ -4,23 +4,24 @@ extern crate rand;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 extern crate chrono;
+extern crate serde_json;
 
-use std::io::prelude::*;
 use chrono::prelude::*;
-use std::io;
-use std::fs;
-use std::fmt;
-use std::path::{Path, PathBuf};
+use petgraph::dot::{Config, Dot};
 use petgraph::prelude::*;
-use petgraph::dot::{Dot, Config};
 use petgraph_graphml::GraphMl;
+use std::fmt;
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 
+use rand::Rng;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::hash_map::RandomState;
-use std::hash::{Hash, Hasher, BuildHasher};
-use rand::Rng;
+use std::collections::HashMap;
+use std::hash::{BuildHasher, Hash, Hasher};
 
 pub type Graph = petgraph::Graph<Node, (), petgraph::Undirected>;
 
@@ -72,7 +73,10 @@ pub fn read_graph<P: AsRef<Path>>(path: P) -> Result<Graph, serde_json::Error> {
 
 pub fn graph_dir() -> PathBuf {
     let local: DateTime<Local> = Local::now();
-    let dir = PathBuf::from(format!("data/graphs/graph_{}", local.format("%Y-%m-%dT%H-%M-%S")));
+    let dir = PathBuf::from(format!(
+        "data/graphs/graph_{}",
+        local.format("%Y-%m-%dT%H-%M-%S")
+    ));
     if !dir.exists() {
         fs::create_dir_all(&dir).unwrap();
     }
@@ -86,10 +90,12 @@ pub fn write_graph<P: AsRef<Path>>(graph: &Graph, dir: P) -> Result<(), serde_js
     serde_json::to_writer(writer, graph)
 }
 
-
 pub fn export_graph_to_graphml<P: AsRef<Path>>(graph: &Graph, dir: P) -> io::Result<()> {
-    let graphml = GraphMl::new(&graph).pretty_print(true).export_node_weights_display();
-    let file = fs::File::create(dir.as_ref().join("graph.graphml")).expect("Could not create graph/graph.graphml file");
+    let graphml = GraphMl::new(&graph)
+        .pretty_print(true)
+        .export_node_weights_display();
+    let file = fs::File::create(dir.as_ref().join("graph.graphml"))
+        .expect("Could not create graph/graph.graphml file");
     let writer = io::BufWriter::new(file);
     graphml.to_writer(writer)
 }
@@ -97,7 +103,8 @@ pub fn export_graph_to_graphml<P: AsRef<Path>>(graph: &Graph, dir: P) -> io::Res
 pub fn export_graph_to_dot<P: AsRef<Path>>(graph: &Graph, dir: P) -> io::Result<()> {
     let no_edge_data = graph.map(|_, node| node, |_, _| NoEdgeData);
     let exported_graph = Dot::with_config(&no_edge_data, &[Config::EdgeNoLabel]);
-    let file = fs::File::create(dir.as_ref().join("graph.dot")).expect("Could not create graph/graph.dot file");
+    let file = fs::File::create(dir.as_ref().join("graph.dot"))
+        .expect("Could not create graph/graph.dot file");
     let mut buffer = io::BufWriter::new(file);
     write!(&mut buffer, "{}", exported_graph)
 }
@@ -106,7 +113,13 @@ pub fn anonymize_graph(graph: Graph) -> Graph {
     let hash_key = RandomState::new();
     let mut rng = rand::thread_rng();
     let salt = rng.gen::<u64>();
-    graph.map(|_, node| Node { kind: node.kind, id: hash_with_salt(&hash_key, &node.id, salt)}, |_, _| ())
+    graph.map(
+        |_, node| Node {
+            kind: node.kind,
+            id: hash_with_salt(&hash_key, &node.id, salt),
+        },
+        |_, _| (),
+    )
 }
 
 fn is_wellformed_node(graph: &Graph, idx: NodeIndex) -> bool {
@@ -114,33 +127,81 @@ fn is_wellformed_node(graph: &Graph, idx: NodeIndex) -> bool {
         NodeType::User => {
             // a user needs exactly one HS and be a member of at least one room.
             // This should be impossible, as we get the HS from the user id and find users through a room.
-            graph.neighbors(idx).filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Server).count() == 1
-            && graph.neighbors(idx).any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
+            graph
+                .neighbors(idx)
+                .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Server)
+                .count() == 1
+                && graph
+                    .neighbors(idx)
+                    .any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
         },
         NodeType::Room => {
             // A room needs at least one user and at least one server. Could be caused by ignore patterns.
             // As those disconnected rooms do nothing for the simulation an only dillute the results, I should remove them.
-            graph.neighbors(idx).any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
-            && graph.neighbors(idx).any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Server)
+            graph
+                .neighbors(idx)
+                .any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
+                && graph
+                    .neighbors(idx)
+                    .any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Server)
         },
         NodeType::Server => {
             // A server needs at least one user and at least one room.
             // This should be impossible, as we only can see servers through a user in a room.
-            graph.neighbors(idx).any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
-            && graph.neighbors(idx).any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
+            graph
+                .neighbors(idx)
+                .any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
+                && graph
+                    .neighbors(idx)
+                    .any(|neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
         },
     };
     if !is_wellformed {
-        eprintln!("malformed node: {}. neighbors: {} users, {} rooms, {} servers.",
-                  graph[idx],
-                  graph.neighbors(idx).filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::User).count(),
-                  graph.neighbors(idx).filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Room).count(),
-                  graph.neighbors(idx).filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Server).count(),
-                 );
+        eprintln!(
+            "malformed node: {}. neighbors: {} users, {} rooms, {} servers.",
+            graph[idx],
+            graph
+                .neighbors(idx)
+                .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
+                .count(),
+            graph
+                .neighbors(idx)
+                .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
+                .count(),
+            graph
+                .neighbors(idx)
+                .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Server)
+                .count(),
+        );
     }
     is_wellformed
 }
 
 pub fn is_wellformed_graph(graph: &Graph) -> bool {
-    graph.node_indices().all(|idx| is_wellformed_node(graph, idx))
+    graph
+        .node_indices()
+        .all(|idx| is_wellformed_node(graph, idx))
+}
+
+// returns map from server id to number of users and rooms
+pub fn user_room_distribution(graph: &Graph) -> HashMap<u64, (usize, usize)> {
+    graph
+        .node_indices()
+        .filter(|idx| graph[*idx].kind == NodeType::Server)
+        .map(|server_idx| {
+            (
+                graph[server_idx].id,
+                (
+                    graph
+                        .neighbors(server_idx)
+                        .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::User)
+                        .count(),
+                    graph
+                        .neighbors(server_idx)
+                        .filter(|&neighbor_idx| graph[neighbor_idx].kind == NodeType::Room)
+                        .count(),
+                ),
+            )
+        })
+        .collect()
 }
